49db6a0b93507a1b4e58f0c572ae26ac8da58f97
Carson Bain <carson@felixforyou.ca>
2023-01-04 09:40:43 -0500

    a start

---
+++ app.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-col h-full">
+    <div class="w-full max-w-screen-lg py-10 px-6 md:px-16 mx-auto flex-1">
+      <header>
+        <nav class="flex justify-between items-center mb-12">
+          <NuxtLink class="font-extrabold text-xl md:text-3xl uppercase tracking-wide" to="/">Carson Bain</NuxtLink>
+          <div>
+            <ul class="flex list-none space-x-3">
+              <li>
+                <NuxtLink
+                  class="md:text-lg font-semibold py-1"
+                  activeClass="border-b-2 border-solid border-current"
+                  to="/"
+                >
+                  Work
+                </NuxtLink>
+              </li>
+              <li>
+                <NuxtLink
+                  class="md:text-lg font-semibold py-1"
+                  activeClass="border-b-2 border-solid border-current"
+                  to="/about"
+                >
+                  About
+                </NuxtLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <main class="relative mt-16">
+        <NuxtPage></NuxtPage>
+      </main>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-12">
+    <NuxtLink v-for="index in 4" class="relative group flex" :key="index" :to="`/work/${index}`">
+      <div class="flex flex-col">
+        <!-- TODO: use nuxt-picture instead? -->
+        <NuxtImg src="/test.jpg" />
+        <div class="text-black text-2xl md:text-3xl font-semibold mt-6">— Mallorca, Spain</div>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
