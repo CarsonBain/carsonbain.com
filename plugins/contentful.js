43d232c2319ced6d5aa16efc0594d889a834430a
Carson Bain <carson@felixforyou.ca>
2023-01-06 09:52:49 -0500

    fix: try to fix 500

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,11 +1,11 @@
-import * as contentfulServer from 'contentful';
+import * as contentful from 'contentful';
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
   const configObject = {
     space: config.public.contentfulSpaceId,
     accessToken: config.public.contentfulAccessToken,
   };
-  const client = contentfulServer.createClient(configObject);
+  const client = contentful.createClient(configObject);
   return {
     provide: {
       contentful: client,
