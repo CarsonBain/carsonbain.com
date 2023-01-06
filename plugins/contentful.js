de69baffcf4ffcdf79ee97c8652779d3de49c4f7
Carson Bain <carson@felixforyou.ca>
2023-01-06 13:48:19 -0500

    fix: stupid change for prod deploys

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,11 +1,14 @@
-import * as contentful from 'contentful';
+import { createClient } from 'contentful';
+import contentful from 'contentful';
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
+  // For some reason you have to do this for prod vs dev...https://github.com/contentful/contentful.js/issues/1233#issuecomment-1216175360
+  const createClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient;
   const configObject = {
     space: config.public.contentfulSpaceId,
     accessToken: config.public.contentfulAccessToken,
   };
-  const client = contentful.createClient(configObject);
+  const client = createClientFunc(configObject);
   return {
     provide: {
       contentful: client,
