0f989f6b2329dbe2dea3fe1f1ca1da28f53cce47
Carson Bain <carson@felixforyou.ca>
2023-01-11 10:54:34 -0500

    fix: update copy

--- pages/about.vue
+++ pages/about.vue
@@ -3,8 +3,8 @@
     <p class="max-w-prose">
       Carson Bain is a photographer from Toronto, Ontario. Currently he is working as a front end developer at
       <a class="underline" href="https://www.felixforyou.ca/" target="_blank">Felix</a>, and some of his writing on
-      development can be found on his
-      <a class="underline" href="https://dev.carsonbain.com/blog" target="_blank">software blog</a>.
+      software development can be found on his
+      <a class="underline" href="https://dev.carsonbain.com/blog" target="_blank">blog</a>.
     </p>
   </div>
 </template>
