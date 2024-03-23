b29e36df6fba6a3d250d4381a6266505891bc9f1
Carson Bain <carson@felixforyou.ca>
2024-03-23 12:53:28 -0400

    fix: swap order

--- pages/writing/index.vue
+++ pages/writing/index.vue
@@ -4,11 +4,11 @@
       <NuxtLink v-for="article in list" :key="article._path" :to="article._path">
         <h2 class="font-medium md:text-base">{{ article.title }}</h2>
         <p class="text-xs mt-1 flex gap-x-2 items-center">
-          <span>
-            {{ article.createdAt.slice(0, 10) }}
-          </span>
           <span class="text-gray-600 bg-gray-50 rounded-md px-2 py-1 border-gray-100 border">
             {{ formatCategoryName(article.category) }}
+          </span>
+          <span>
+            {{ article.createdAt.slice(0, 10) }}
           </span>
         </p>
       </NuxtLink>
