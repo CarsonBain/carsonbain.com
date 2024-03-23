53380152a4cb108673600b48cffcdf265fadfd5d
Carson Bain <carson@felixforyou.ca>
2024-03-23 00:02:25 -0400

    fix: more styles

--- app.vue
+++ app.vue
@@ -3,12 +3,12 @@
     <div class="w-full max-w-screen-lg pt-10 pb-16 px-6 md:px-16 mx-auto flex flex-col flex-1">
       <header class="flex-grow-0 flex-shrink-0 basis-auto">
         <nav class="flex justify-between items-center">
-          <NuxtLink class="font-extrabold text-xl md:text-3xl uppercase tracking-wide" to="/">Carson Bain</NuxtLink>
+          <NuxtLink class="font-bold text-xl md:text-3xl uppercase tracking-wider" to="/">Carson Bain</NuxtLink>
           <div>
             <ul class="flex list-none space-x-3">
               <li>
                 <NuxtLink
-                  class="md:text-lg font-semibold py-1"
+                  class="md:text-lg font-medium py-1"
                   activeClass="border-b-2 border-solid border-current"
                   to="/"
                 >
@@ -17,7 +17,7 @@
               </li>
               <li>
                 <NuxtLink
-                  class="md:text-lg font-semibold py-1"
+                  class="md:text-lg font-medium py-1"
                   activeClass="border-b-2 border-solid border-current"
                   to="/writing"
                 >
@@ -26,7 +26,7 @@
               </li>
               <li>
                 <NuxtLink
-                  class="md:text-lg font-semibold py-1"
+                  class="md:text-lg font-medium py-1"
                   activeClass="border-b-2 border-solid border-current"
                   to="/about"
                 >
