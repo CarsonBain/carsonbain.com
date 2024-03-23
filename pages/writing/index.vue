b0d71ad63c363236f284b9261bd35a2ba4921948
Carson Bain <carson@felixforyou.ca>
2024-03-22 23:37:07 -0400

    fix: more styles

--- pages/writing/index.vue
+++ pages/writing/index.vue
@@ -1,13 +1,13 @@
 <template>
   <ContentList path="/writing" v-slot="{ list }">
-    <div class="w-full flex flex-col gap-y-8 md:gap-y-10">
+    <div class="w-full flex flex-col gap-y-8 md:gap-y-8">
       <NuxtLink v-for="article in list" :key="article._path" :to="article._path">
         <h2 class="font-medium">{{ article.title }}</h2>
-        <p class="text-xs mt-2 flex gap-x-1">
-          <span class="text-gray-600 bg-gray-50 rounded px-2 py-1">
+        <p class="text-xs mt-1 flex gap-x-2 items-center">
+          <span>
             {{ article.createdAt.slice(0, 10) }}
           </span>
-          <span class="text-gray-600 bg-gray-50 rounded px-2 py-1">
+          <span class="text-gray-600 bg-gray-50 rounded-md px-2 py-1">
             {{ formatCategoryName(article.category) }}
           </span>
         </p>
