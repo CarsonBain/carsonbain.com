5ad0b0523303c6bb3c8b7df41e868be5d6031fde
Carson Bain <carson@felixforyou.ca>
2024-03-22 23:48:22 -0400

    fix: update about

--- pages/about.vue
+++ pages/about.vue
@@ -5,29 +5,11 @@
       healthcare products at
       <a class="underline" href="https://www.felixforyou.ca/" target="_blank">Felix</a>.
     </p>
+
     <p class="mt-4">
-      <a class="flex space-x-2 items-center underline" href="mailto:carsonbain@gmail.com">
-        <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24">
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="1.5"
-            d="M4.75 7.75C4.75 6.64543 5.64543 5.75 6.75 5.75H17.25C18.3546 5.75 19.25 6.64543 19.25 7.75V16.25C19.25 17.3546 18.3546 18.25 17.25 18.25H6.75C5.64543 18.25 4.75 17.3546 4.75 16.25V7.75Z"
-          ></path>
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="1.5"
-            d="M5.5 6.5L12 12.25L18.5 6.5"
-          ></path>
-        </svg>
-        carsonbain@gmail.com</a
-      >
-    </p>
-    <p class="mt-2">
-      <a class="underline" target="_blank" href="https://www.instagram.com/carsonbain/">@carsonbain</a>
+      <a class="underline" href="mailto:carsonbain@gmail.com"> email</a>,
+      <a class="underline" target="_blank" href="https://www.instagram.com/carsonbain/">instagram</a>,
+      <a class="underline" target="_blank" href="https://github.com/CarsonBain">github</a>
     </p>
   </div>
 </template>
