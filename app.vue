0078129e3251436f031e378885e6a0fbbb9c6e53
Carson Bain <carsonbain@Carsons-MacBook-Air.local>
2024-03-24 20:49:39 -0400

    fix: small viewport units?

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col h-full min-h-dvh text-gray-950 text-sm">
+  <div class="flex flex-col h-full min-h-svh text-gray-950 text-sm">
     <div class="w-full max-w-screen-lg pt-10 pb-16 px-6 md:px-16 mx-auto flex flex-col flex-1">
       <header class="flex-grow-0 flex-shrink-0 basis-auto">
         <nav class="flex justify-between items-center flex-wrap gap-x-4 gap-y-2">
